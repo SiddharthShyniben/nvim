9a9c20e4bc24315fef627107c4f8439b6c5ab8a0
SiddharthShyniben <siddharth.muscat@gmail.com>
2022-01-21 10:23:53 +0530

    Add typescript language server

--- update-lsp.zsh
+++ update-lsp.zsh
@@ -2,3 +2,4 @@ npm install -g @angular/language-server
 npm i -g bash-language-server
 npm i -g vscode-langservers-extracted
 brew install lua-language-server
+npm install -g typescript-language-server
