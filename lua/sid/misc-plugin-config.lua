5f2f344092aa56bea4f6b82b2b10bcbbd0b3d154
SiddharthShyniben <siddharth.muscat@gmail.com>
2022-01-21 14:19:40 +0530

    Single quotes and smooth scroll

--- lua/sid/misc-plugin-config.lua
+++ lua/sid/misc-plugin-config.lua
@@ -2,17 +2,19 @@ vim.g.javascript_plugin_jsdoc = 1
 vim.g.javascript_plugin_ngdoc = 1
 vim.g.javascript_plugin_flow  = 1
 
-vim.g.javascript_conceal_function             = "ƒ"
-vim.g.javascript_conceal_null                 = "ø"
-vim.g.javascript_conceal_this                 = "@"
-vim.g.javascript_conceal_return               = "⇚"
-vim.g.javascript_conceal_undefined            = "¿"
-vim.g.javascript_conceal_NaN                  = "ℕ"
-vim.g.javascript_conceal_prototype            = "¶"
-vim.g.javascript_conceal_static               = "•"
-vim.g.javascript_conceal_super                = "Ω"
-vim.g.javascript_conceal_arrow_function       = "⇒"
-vim.g.javascript_conceal_noarg_arrow_function = "🞅"
-vim.g.javascript_conceal_underscore_arrow_function = "🞅"
+vim.g.javascript_conceal_function             = 'ƒ'
+vim.g.javascript_conceal_null                 = 'ø'
+vim.g.javascript_conceal_this                 = '@'
+vim.g.javascript_conceal_return               = '⇚'
+vim.g.javascript_conceal_undefined            = '¿'
+vim.g.javascript_conceal_NaN                  = 'ℕ'
+vim.g.javascript_conceal_prototype            = '¶'
+vim.g.javascript_conceal_static               = '•'
+vim.g.javascript_conceal_super                = 'Ω'
+vim.g.javascript_conceal_arrow_function       = '⇒'
+vim.g.javascript_conceal_noarg_arrow_function = '🞅'
+vim.g.javascript_conceal_underscore_arrow_function = '🞅'
 
 vim.g.markdown_fenced_languages = {'html', 'css', 'js=javascript', 'jsx=javascript', 'ts=typescript', 'tsx=typescript', 'json', 'yaml', 'toml', 'xml'}
+vim.g.comfortable_motion_scroll_down_key = 'j'
+vim.g.comfortable_motion_scroll_up_key = 'k'
