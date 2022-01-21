f9dc52701f9413915c45e1026e8494a486052021
SiddharthShyniben <siddharth.muscat@gmail.com>
2022-01-21 09:50:23 +0530

    Auto install packer

--- lua/sid/plugins.lua
+++ lua/sid/plugins.lua
@@ -1,3 +1,9 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+	packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup({
@@ -63,6 +69,10 @@ return require('packer').startup({
 		-- misc
 		use 'jiangmiao/auto-pairs'
 		use 'github/copilot.vim'
+
+		if packer_bootstrap then
+			require('packer').sync()
+		end
 	end,
 	config = {
 		display = {
