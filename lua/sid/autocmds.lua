31a32619347c305b8558b390217dd54948ea156f
SiddharthShyniben <siddharth.muscat@gmail.com>
2022-02-03 11:58:25 +0530

    Remove annoying autocmds

--- lua/sid/autocmds.lua
+++ lua/sid/autocmds.lua
@@ -1,36 +1,3 @@
--- TODO convert to lua
-
-vim.api.nvim_exec([[
-	function! Prose()
-		call lexical#init()
-		call litecorrect#init()
-		call textobj#sentence#init()
-		execute ':DittoOn'
-
-		" manual reformatting shortcuts
-		nnoremap <buffer> <silent> Q gqap
-		xnoremap <buffer> <silent> Q gq
-		nnoremap <buffer> <silent> <leader>Q vapJgqap
-
-		" force top correction on most recent misspelling
-		nnoremap <buffer> <c-s> [s1z=<c-o>
-		inoremap <buffer> <c-s> <c-g>u<Esc>[s1z=`]A<c-g>u
-
-		" open most folds
-		setlocal foldlevel=6
-
-		" replace typographical quotes (reedes/vim-textobj-quote)
-		map <silent> <buffer> <leader>qc <Plug>ReplaceWithCurly
-		map <silent> <buffer> <leader>qs <Plug>ReplaceWithStraight
-
-		" highlight words (reedes/vim-wordy)
-		noremap <silent> <buffer> <leader>w :<C-u>NextWordy<cr>
-		xnoremap <silent> <buffer> <leader>w :<C-u>NextWordy<cr>
-	endfunction
-
-	" automatically initialize buffer by file type
-	autocmd FileType markdown,mkd,text call Prose()
-
-	" invoke manually by command for other file types
-	command! -nargs=0 Prose call Prose()
-]], true)
+vim.cmd[[
+autocmd BufWritePre *.lua source ~/.config/nvim/init.lua
+]]
