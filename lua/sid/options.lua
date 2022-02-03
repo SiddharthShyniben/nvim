ae26faf9d616e6424d9855ecc678210f35873531
SiddharthShyniben <siddharth.muscat@gmail.com>
2022-02-03 11:54:36 +0530

    Fine tune options

--- lua/sid/options.lua
+++ lua/sid/options.lua
@@ -1,32 +1,61 @@
 local function set(x)
-	vim.cmd('set ' .. x)
+	local output = pcall(function () vim.cmd('set ' .. x) end)
+	if not output then print('Error setting ' .. x) end
 end
 
 set 'tabstop=4'
 set 'shiftwidth=4'
 set 'noexpandtab'
+set 'smarttab'
 set 'cindent'
-set 'conceallevel=1'
+
 set 'number'
+
 set 'undofile'
 set 'undodir=~/.config/nvim/undo'
 set 'undolevels=1000'
+
 set 'foldlevelstart=99'
+set 'scrolloff=999'
+set 'conceallevel=2'
+
 set 'nowrap'
 set 'mouse=a'
+
 set 'formatoptions-=ro'
+set 'formatoptions+=2nja'
+
 set 'completeopt=menu,menuone,noselect'
+
 set 'ignorecase smartcase'
-set 'nomodeline'
 set 'splitbelow splitright'
+
+set 'nomodeline'
+
 set 'confirm'
-set 'scrolloff=999'
+
 set 'colorcolumn=80'
 set 'textwidth=80'
-set 'termguicolors'
-set 'fillchars=eob:'
-set 'nohidden' -- way too much confusion. when i am a better vimmer i might use this
-set 'writebackup' -- i have a habit of messing up stuff and not having it in vcs
-set 'smarttab'
+set 'cmdheight=2'
 set 'showtabline=1'
+set 'laststatus=2'
+
+set 'termguicolors'
+
+set 'writebackup' -- i have a habit of messing up stuff and not having it in vcs
+
 set 'errorbells'
+
+set 'signcolumn=yes'
+
+set 'list listchars=multispace:,lead:,trail:,precedes:ﲖ,extends:ﲕ'
+set 'fillchars=eob:'
+
+set 'cursorline cursorlineopt=both'
+set 'cursorcolumn'
+
+set 'winminheight=0 winminwidth=0'
+
+set 'scrollbind cursorbind'
+
+set 'title'
