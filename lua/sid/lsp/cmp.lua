fdebdc33ba0d41710595e3e330641de9b96ccaf8
Siddharth Shyniben <siddhart.muscat@gmail.com>
2023-05-20 11:16:02 +0530

    Make LSP work

--- lua/sid/lsp/cmp.lua
+++ lua/sid/lsp/cmp.lua
@@ -10,9 +10,8 @@ cmp.setup {
 		end,
 	},
 	mapping = cmp.mapping.preset.insert({
-		['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
-		['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
-		-- C-b (back) C-f (forward) for snippet placeholder navigation.
+		['<C-u>'] = cmp.mapping.scroll_docs(-4),
+		['<C-d>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<CR>'] = cmp.mapping.confirm {
 			behavior = cmp.ConfirmBehavior.Replace,
@@ -37,13 +36,12 @@ cmp.setup {
 			end
 		end, {'i', 's'}),
 	}),
-	sources = {{
+	sources = {
 		{name = 'nvim_lsp'},
 		{name = 'luasnip'},
-	}, {
 		{name = 'buffer'},
 		{name = 'path'},
-	}},
+	},
 }
 
 cmp.setup.filetype('gitcommit', {
--- lua/sid/lsp/mason.lua
+++ lua/sid/lsp/mason.lua
@@ -1,7 +1,7 @@
 require('neodev').setup()
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local lspconfig = require('lspconfig')
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- TODO: HTML, CSS, JS, Rust, Node.js, Angular, Svelte, tsserver
 local servers = {
@@ -49,11 +49,11 @@ mason_lspconfig.setup {
 }
 
 mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    }
-  end,
+	function(server_name)
+		require('lspconfig')[server_name].setup {
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = servers[server_name],
+		}
+	end,
 }
