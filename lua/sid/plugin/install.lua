99f12c033953c772f2aa78c275c348659cec8462
Siddharth Shyniben <siddhart.muscat@gmail.com>
2023-11-22 06:35:33 +0530

    Add neorg

--- lua/sid/plugin/install.lua
+++ lua/sid/plugin/install.lua
@@ -118,6 +118,27 @@ require('lazy').setup({
 	-- 	})
 	-- end},
 
+	{
+		"nvim-neorg/neorg",
+		build = ":Neorg sync-parsers",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("neorg").setup {
+				load = {
+					["core.defaults"] = {}, -- Loads default behaviour
+					["core.concealer"] = {}, -- Adds pretty icons to your documents
+					["core.dirman"] = { -- Manages Neorg workspaces
+						config = {
+							workspaces = {
+								notes = "~/notes",
+							},
+						},
+					},
+				},
+			}
+		end,
+	},
+
 	-- Fun
 	{'eandrju/cellular-automaton.nvim', lazy = true},
 }, {install = {colorscheme = {'tokyonight-night'}}})
